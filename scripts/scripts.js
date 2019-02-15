2e3dae62fe5d1447bafbddd85ea14cf2c25316ad
Luke Forthman <lukeforthman96@gmail.com>
2019-02-14 22:14:22 -0700

    Fixed api to deal with response format. Pulling properly now.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,25 @@
 document.getElementById("refreshSubmit").addEventListener("click", function(event){
   event.preventDefault();
-
+var proxyURL = "https://cors-anywhere.herokuapp.com/";
   const url = "http://rsbuddy.com/exchange/summary.json";
-  fetch(url, {'mode': 'no-cors'})
+  fetch(proxyURL + url)
   .then(function(response){
     return response.json();
-  }).then(function(json){
-    let results = "<div class=\"container\">";
-    for(let i = 0; i < json.list.length; i++){
-      results += "<div class=\"row boxy-small\">";
-      results += "<img src=\"http://services.runescape.com/m=itemdb_oldschool/1548934210902_obj_big.gif?id=" + json.list[i].id + "\" class=\"daily\">";
-      results += "<p class=\"daily\">" + json.list[i].name + "</p>";
+    }).then(function(json){
+      let results = "<div class=\"container\">";
+      console.log(json);
+      for(var key in json){
+        if(json[key].overall_quantity > 10000){
+          results += "<div class=\"row boxy-small\">";
+          results += "<img src=\"http://services.runescape.com/m=itemdb_oldschool/1548934210902_obj_big.gif?id=" + json[key].id + "\" class=\"daily\">";
+          results += "<p class=\"daily\"> <b>" + json[key].name + "</b>&nbsp;</p><br>";
+          results += "<p class= \"daily\">Buy Price:&nbsp;" + json[key].buy_average + "&nbsp;</p>";
+          results += "<p class= \"daily\">Sell Price:&nbsp;" + json[key].sell_average + "&nbsp;</p>";
+          results += "<p class= \"daily\"><b>Profit:</b>&nbsp;" + (json[key].sell_average - json[key].buy_average) + "&nbsp;</p>";
+          results += "</div>";
+        }
+      }
       results += "</div>";
-    }
-    results += "</div>";
-    document.getElementById("results").innerHTML = results;
-  });
+      document.getElementById("results").innerHTML = results;
+    });
 });
